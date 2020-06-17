72437c0967ef239fb10cb487fd588a02e363f8ac
Megan O'Bryan <megan@codehs.com>
2020-06-17 14:41:56 -0700

    Added models for UserProfile and Bunks

--- jitterbunk/bunk/models.py
+++ jitterbunk/bunk/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    photo_url = models.URLField()
+
+class Bunk(models.Model):
+    from_user = models.ForeignKey(UserProfile, on_delete=models.PROTECT, related_name="from_user")
+    to_user = models.ForeignKey(UserProfile, on_delete=models.PROTECT, related_name="to_user")
+    time_sent = models.DateTimeField(auto_now_add=True,
+                                   help_text="The date and time this bunk was sent.")
+
