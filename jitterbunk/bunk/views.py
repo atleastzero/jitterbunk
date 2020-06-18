189ae0a536e247a2fa053427e90838512b1e083c
Megan O'Bryan <megan@codehs.com>
2020-06-17 19:08:16 -0700

    Started working on IndecxView (Main Bunk Feed)

--- jitterbunk/bunk/views.py
+++ jitterbunk/bunk/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Bunk
+
+class IndexView(generic.ListView):
+    template_name = 'bunk/index.html'
+    context_object_name = 'bunk_list'
+
+    def get_queryset(self):
+        """Return all bunks."""
+        return Bunk.objects.order_by('-time_sent')[:20]
