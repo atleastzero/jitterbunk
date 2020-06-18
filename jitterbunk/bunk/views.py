3aedf4d73e8073bcd95385b1e083f01cc84abd43
Megan O'Bryan <megan@codehs.com>
2020-06-18 09:13:41 -0700

    Added links, profile images

--- jitterbunk/bunk/views.py
+++ jitterbunk/bunk/views.py
@@ -13,12 +13,11 @@ class IndexView(generic.ListView):
 class UserProfileView(generic.DetailView):
     model = UserProfile
     template_name = 'bunk/user_feed.html'
-
-    # def get_object(self, queryset=None):
-    #     return self.kwargs['slug']
+    context_object_name = 'user'
 
     def get_context_data(self, **kwargs):
         context = super(UserProfileView, self).get_context_data(**kwargs)
         bunks_recieved = Bunk.objects.filter(to_user=self.get_object())
         bunks_sent = Bunk.objects.filter(from_user=self.get_object())
         context["bunks_with_user"] = (bunks_recieved | bunks_sent).order_by('-time_sent')[:20]
+        return context
